56d7c53ceb7a2f25da031bacdfed91e167bd6956
elliisabeta <142780441+Ellisabeta@users.noreply.github.com>
2024-12-02 16:25:19 +0100

    added another Override in the super class because it was giving me the hash code of the list and not the actual array values

--- V1/Medien.java
+++ V1/Medien.java
@@ -17,14 +17,21 @@ public class Medien {
         Preis = preis;
     }
 
+    @Override
+    public String toString() {
+        return "Titel: " + Titel + ", Jahr: " + Jahr + ", Preis: " + Preis + ", ";
+    }
+
     public static void main(String[] args) {
-        System.out.println("Medien");
+        System.out.println("MedienListe");
 
         ArrayList<Medien> medienList = new ArrayList<Medien>();
         medienList.add(new DVDs("dvd1", 2010, 1.50, 90));
         medienList.add(new CDs("cd1", 2000, 2.00, "My cool bandname hehe"));
         medienList.add(new Buecher("buch1", 2020, 3.00, 1010101010));
 
-        System.out.println(medienList);
+        for (Medien medien : medienList) {
+            System.out.println(medien);
+        }
     }
 }
