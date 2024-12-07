e066db03f9e6e4d9bd303bf7481639eb8b09909d
elliisabeta <142780441+Ellisabeta@users.noreply.github.com>
2024-12-07 14:31:56 +0100

    created structure of the passengers and flight classes

---
+++ src/main/java/D2/Flug.java
@@ -0,0 +1,25 @@
+package D2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Flug {
+
+    List<Passagier> passengers = new ArrayList<Passagier>();
+
+    public void addPassenger() {
+
+    }
+
+    public void removePassenger() {
+
+    }
+
+    public void printPassengerList() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/D2/Passagier.java
@@ -0,0 +1,14 @@
+package D2;
+
+public class Passagier {
+
+    private String name;
+
+    public void printName() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
