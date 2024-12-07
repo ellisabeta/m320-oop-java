59fb588550e80ac48ec2da10f55033697e427bb2
elliisabeta <142780441+Ellisabeta@users.noreply.github.com>
2024-12-07 16:15:19 +0100

    adjusted all three methods

--- src/main/java/D2/Flug.java
+++ src/main/java/D2/Flug.java
@@ -8,15 +8,17 @@ public class Flug {
     List<Passagier> passengers = new ArrayList<Passagier>();
 
     public void addPassenger() {
-
+        passengers.add(new Passagier());
     }
 
     public void removePassenger() {
-
+        passengers.remove(new Passagier());
     }
 
     public void printPassengerList() {
-
+        for (Passagier p : passengers) {
+            p.printName();
+        }
     }
 
     public static void main(String[] args) {
