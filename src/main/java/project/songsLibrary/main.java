5fce30b062a41e920951cdc0f95a5d2008ee2de1
elliisabeta <142780441+Ellisabeta@users.noreply.github.com>
2025-01-21 20:01:56 +0100

    main file created and adjusted for spring boot application to scan for main class when running

--- src/main/java/project/songsLibrary/main.java
+++ src/main/java/project/songsLibrary/main.java
@@ -1,5 +1,11 @@
-package Kompetenzen;
+package project.songsLibrary;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class main {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        SpringApplication.run(main.class, args);
+    }
 }
