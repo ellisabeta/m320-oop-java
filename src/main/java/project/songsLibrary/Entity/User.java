845a2c2aa428178a201dfbade05232d6973cf960
elliisabeta <142780441+Ellisabeta@users.noreply.github.com>
2025-01-22 22:47:04 +0100

    without the Table annotation name users it was giving me DDL Syntax Error at User, because it wasnt recognising it as a table but a variable, which caused errors when running

--- src/main/java/project/songsLibrary/Entity/User.java
+++ src/main/java/project/songsLibrary/Entity/User.java
@@ -3,8 +3,10 @@ package project.songsLibrary.Entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id
