58dce11b4e62b790b78980baf4426378d805f580
elliisabeta <142780441+Ellisabeta@users.noreply.github.com>
2025-01-22 22:43:36 +0100

    adjusted that main class works

--- src/main/java/project/songsLibrary/mainApplication.java
+++ src/main/java/project/songsLibrary/mainApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class main {
+public class mainApplication {
     public static void main(String[] args) {
-        SpringApplication.run(main.class, args);
+        SpringApplication.run(mainApplication.class, args);
     }
 }
