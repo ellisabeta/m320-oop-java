f86a446ad3307d858ec8ba66d358f9f565653a9f
elliisabeta <142780441+Ellisabeta@users.noreply.github.com>
2025-01-24 13:53:36 +0100

    allowing backend to listen to requests coming from frontend

--- src/main/java/project/songsLibrary/Config.java
+++ src/main/java/project/songsLibrary/Config.java
@@ -1,4 +1,13 @@
 package project.songsLibrary;
 
-public class Config {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class Config implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**").allowedOrigins("http://localhost:5173");
+    }
 }
