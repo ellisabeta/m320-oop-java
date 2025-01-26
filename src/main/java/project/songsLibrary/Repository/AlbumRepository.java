7b6be3be9428e7211bfb41a200ddcc21988cd1d2
elliisabeta <142780441+Ellisabeta@users.noreply.github.com>
2025-01-26 19:46:32 +0100

    created Repositories for every entity

--- src/main/java/project/songsLibrary/Repository/AlbumRepository.java
+++ src/main/java/project/songsLibrary/Repository/AlbumRepository.java
@@ -1,4 +1,7 @@
 package project.songsLibrary.Repository;
 
-public interface AlbumRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import project.songsLibrary.Entity.Album;
+
+public interface AlbumRepository extends JpaRepository<Album, Integer> {
 }
--- src/main/java/project/songsLibrary/Repository/PlaylistRepository.java
+++ src/main/java/project/songsLibrary/Repository/PlaylistRepository.java
@@ -1,4 +1,7 @@
 package project.songsLibrary.Repository;
 
-public class PlaylistRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import project.songsLibrary.Entity.Playlist;
+
+public interface PlaylistRepository extends JpaRepository<Playlist, Integer> {
 }
