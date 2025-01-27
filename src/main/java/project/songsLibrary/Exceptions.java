381c0661b43d2e2cfd1f231ec1f6b67a6a184d67
elliisabeta <142780441+Ellisabeta@users.noreply.github.com>
2025-01-27 11:52:10 +0100

    renamed the exception

--- src/main/java/project/songsLibrary/Exceptions.java
+++
@@ -1,7 +0,0 @@
-package project.songsLibrary.Exceptions;
-
-public class Exceptions extends Exception {
-    public Exceptions(String message) {
-        super(message);
-    }
-}
---
+++ src/main/java/project/songsLibrary/MyException.java
@@ -0,0 +1,7 @@
+package project.songsLibrary;
+
+public class MyException extends Exception {
+    public MyException(String message) {
+        super(message);
+    }
+}
