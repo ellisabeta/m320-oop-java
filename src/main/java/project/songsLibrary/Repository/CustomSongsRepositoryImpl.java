a1dbb8c94f254f4b3b0ccd2d1c2365c60437e1b1
elliisabeta <142780441+Ellisabeta@users.noreply.github.com>
2025-01-27 22:32:41 +0100

    I commented on most lines for clarity of what i am trying to achieve with these two methods. Since i use the jpql queries i also have the parameters setting in the value and so that it matches. Just documented in the code what each logic line does and its functionality

--- src/main/java/project/songsLibrary/Repository/CustomSongsRepositoryImpl.java
+++ src/main/java/project/songsLibrary/Repository/CustomSongsRepositoryImpl.java
@@ -13,20 +13,27 @@ public class CustomSongsRepositoryImpl implements CustomSongsRepository {
     @PersistenceContext
     private EntityManager entityManager;
 
+    // This method will find songs by the title, which is case-sensitive
     @Override
     public List<Songs> findSongsByTitleContaining(String keyword) {
+        // A jpql statement to find the typed in keyword from songs entity
         String jpql = "SELECT s FROM Songs s WHERE s.songsName LIKE :keyword";
+        // I set here a value to the keyword and then get the list in the createQuery which uses the string statement from above
         return entityManager.createQuery(jpql, Songs.class)
+                // using the % sign it matches all the options of what the query found with the keyword that the user types in
                 .setParameter("keyword", "%" + keyword + "%")
                 .getResultList();
     }
 
     @Override
     public int countSongsByDurationGreaterThan(int duration) {
+        // Here is again a jpql statement to find from songs the duration which is greater that the duration that I set
         String jpql = "SELECT COUNT(s) FROM Songs s WHERE s.songsDuration > :duration";
+        // from the string above it creates a Query and then set the value for duration
         Long count = entityManager.createQuery(jpql, Long.class)
                 .setParameter("duration", duration)
                 .getSingleResult();
+        // convert my count variable from Long to int
         return count.intValue();
     }
 }
