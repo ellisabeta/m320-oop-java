cb9d97fb78046b7d16646e7ce57b7094c67d5713
elliisabeta <142780441+Ellisabeta@users.noreply.github.com>
2025-01-27 20:43:40 +0100

    originally created this custom interface which is being implemented by the class CustomSongsRepositoryImpl for statistics. Didnt get the time to do statistics with showing songs titles and their durations

--- src/main/java/project/songsLibrary/Repository/CustomSongsRepository.java
+++ src/main/java/project/songsLibrary/Repository/CustomSongsRepository.java
@@ -1,4 +1,11 @@
 package project.songsLibrary.Repository;
 
+import project.songsLibrary.Entity.Songs;
+
+import java.util.List;
+
 public interface CustomSongsRepository {
+
+    List<Songs> findSongsByTitleContaining(String keyword);
+    int countSongsByDurationGreaterThan(int duration);
 }
--- src/main/java/project/songsLibrary/Repository/CustomSongsRepositoryImpl.java
+++ src/main/java/project/songsLibrary/Repository/CustomSongsRepositoryImpl.java
@@ -1,4 +1,32 @@
 package project.songsLibrary.Repository;
 
-public class CustomSongsRepositoryImpl {
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import project.songsLibrary.Entity.Songs;
+
+import java.util.List;
+
+// Initially wrote this class for statistics to show songs title and durations
+public class CustomSongsRepositoryImpl implements CustomSongsRepository {
+
+    // This is an annotation that I added to interact with my database as read and write the queries.
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public List<Songs> findSongsByTitleContaining(String keyword) {
+        String jpql = "SELECT s FROM Songs s WHERE s.songsName LIKE :keyword";
+        return entityManager.createQuery(jpql, Songs.class)
+                .setParameter("keyword", "%" + keyword + "%")
+                .getResultList();
+    }
+
+    @Override
+    public int countSongsByDurationGreaterThan(int duration) {
+        String jpql = "SELECT COUNT(s) FROM Songs s WHERE s.songsDuration > :duration";
+        Long count = entityManager.createQuery(jpql, Long.class)
+                .setParameter("duration", duration)
+                .getSingleResult();
+        return count.intValue();
+    }
 }
